add950a9e1f5d6f2b0db6e3771b8e908ba49a2a0
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2023-11-06 18:59:20 -0500

    Updated Coordinate.java
    Added parseString to Coordinate.java and fixed Lombok annotation to work for tests

--- src/main/java/com/chess/api/model/Coordinate.java
+++ src/main/java/com/chess/api/model/Coordinate.java
@@ -1,9 +1,13 @@
 package com.chess.api.model;
 
+import java.util.Locale;
+import lombok.Getter;
+
+@Getter
 public class Coordinate {
 
-    int posX;
-    int posY;
+    private final int posX;
+    private final int posY;
 
     public Coordinate(int x, int y) {
         if (x > 7 || x < 0 || y > 7 || y < 0) {
@@ -12,6 +16,16 @@ public class Coordinate {
 
         posX = x;
         posY = y;
+    }
+
+    public static Coordinate parseString(String str) {
+        if (str.length() != 2) {
+            throw new IllegalArgumentException();
+        }
+        char[] chars = str.toLowerCase(Locale.ROOT).toCharArray();
+        int x = chars[0] - 'a';
+        int y = chars[1] - '1';
+        return new Coordinate(x, y);
     }
 
 }
