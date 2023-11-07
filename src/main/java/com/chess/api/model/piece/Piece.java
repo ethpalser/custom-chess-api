6d3fb57e1c9c8927d1d939f63344fe8570036f46
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2023-11-06 23:53:04 -0500

    Created Board.java
    Implemented Board.java with constructor and basic information to track Piece objects

--- src/main/java/com/chess/api/model/piece/Piece.java
+++ src/main/java/com/chess/api/model/piece/Piece.java
@@ -1,8 +1,11 @@
 package com.chess.api.model.piece;
 
+import com.chess.api.model.Colour;
 import com.chess.api.model.Coordinate;
 
 public interface Piece {
+
+    Colour getColour();
 
     Coordinate getCoordinate();
 
