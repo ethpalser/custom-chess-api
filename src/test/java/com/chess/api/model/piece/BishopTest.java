d7c8c2d758bed2cb65b584fd4810a311bd2c0e39
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2023-11-06 21:51:41 -0500

    Update BishopTest.java
    Added tests for setCoordinate

--- src/test/java/com/chess/api/model/piece/BishopTest.java
+++ src/test/java/com/chess/api/model/piece/BishopTest.java
@@ -1,8 +1,7 @@
 package com.chess.api.model.piece;
 
 import com.chess.api.model.Coordinate;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class BishopTest {
@@ -12,6 +11,53 @@ class BishopTest {
         Coordinate coordinate = new Coordinate(2, 0);
         Bishop bishop = new Bishop(coordinate, Colour.WHITE);
         assertNotEquals(null, bishop.getCoordinate());
+        assertFalse(bishop.isMoved());
+    }
+
+    @Test
+    void setCoordinate_toSameLocationAndHasNotMoved_isNotUpdatedAndHasMovedIsFalse() {
+        int x = 2;
+        int y = 0;
+        Coordinate start = new Coordinate(x, y);
+        Bishop bishop = new Bishop(start, Colour.WHITE);
+
+        Coordinate next = new Coordinate(2, 0);
+        bishop.setCoordinate(next);
+        assertEquals(x, bishop.getCoordinate().getX());
+        assertEquals(y, bishop.getCoordinate().getY());
+        assertFalse(bishop.isMoved());
+    }
+
+    @Test
+    void setCoordinate_toSameLocationHasMoved_isNotUpdatedAndHasMovedIsTrue() {
+        int x = 2;
+        int y = 0;
+        Coordinate start = new Coordinate(x, y);
+        Bishop bishop = new Bishop(start, Colour.WHITE);
+
+        int nextX = 3;
+        int nextY = 1;
+        Coordinate moved = new Coordinate(nextX, nextY);
+        bishop.setCoordinate(moved);
+
+        Coordinate next = new Coordinate(nextX, nextY);
+        bishop.setCoordinate(next);
+        assertEquals(nextX, bishop.getCoordinate().getX());
+        assertEquals(nextY, bishop.getCoordinate().getY());
+        assertTrue(bishop.isMoved());
+    }
+
+    @Test
+    void setCoordinate_toNewLocation_isUpdatedAndHasMovedIsTrue() {
+        int x = 2;
+        int y = 0;
+        Coordinate start = new Coordinate(x, y);
+        Bishop bishop = new Bishop(start, Colour.WHITE);
+
+        Coordinate next = new Coordinate(3, 1);
+        bishop.setCoordinate(next);
+        assertEquals(x, bishop.getCoordinate().getX());
+        assertEquals(y, bishop.getCoordinate().getY());
         assertFalse(bishop.isMoved());
     }
     }
