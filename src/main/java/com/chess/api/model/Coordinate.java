e619cb6506fdb195966f312cfd68de7bc9150c64
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2023-11-16 18:18:46 -0500

    Update Coordinate.java
    Added compareTo and equals methods

--- src/main/java/com/chess/api/model/Coordinate.java
+++ src/main/java/com/chess/api/model/Coordinate.java
@@ -4,13 +4,18 @@ import java.util.Locale;
 import lombok.Getter;
 
 @Getter
-public class Coordinate {
+public class Coordinate implements Comparable<Coordinate> {
+
+    private static final int MAX_X = 7;
+    private static final int MAX_Y = 7;
+    private static final int MIN_X = 0;
+    private static final int MIN_Y = 0;
 
     private final int x;
     private final int y;
 
     public Coordinate(int x, int y) {
-        if (x > 7 || x < 0 || y > 7 || y < 0) {
+        if (x > MAX_X || x < MIN_X || y > MAX_Y || y < MIN_Y) {
             throw new IndexOutOfBoundsException();
         }
 
@@ -30,4 +35,15 @@ public class Coordinate {
         return new Coordinate(chars[0], chars[1]);
     }
 
+    @Override
+    public int compareTo(Coordinate o) {
+        if (o == null) {
+            return -1;
+        }
+        return (this.getY() * MAX_X + this.getX()) - (o.getY() * MAX_X + o.getX());
+    }
+
+    public boolean equals(Coordinate coordinate) {
+        return this.compareTo(coordinate) == 0;
+    }
 }
