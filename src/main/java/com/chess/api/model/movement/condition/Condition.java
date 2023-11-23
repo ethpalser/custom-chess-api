de807ba0c0e40c4b9612134f742f712c095c7a23
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2023-11-22 19:03:05 -0500

    Create Condition.java
    Created Condition, which defines the expected value's State relative to the referenced Piece's Property.
    
    Ex. The condition to move a Pawn forward two is as follows:
    Reference = AT_CURRENT
    Property = hasMoved
    Value = Boolean(false)
    State = False

---
+++ src/main/java/com/chess/api/model/movement/condition/Condition.java
@@ -0,0 +1,69 @@
+package com.chess.api.model.movement.condition;
+
+import com.chess.api.model.Board;
+import com.chess.api.model.Coordinate;
+import com.chess.api.model.piece.Piece;
+
+public class Condition {
+
+    private final Reference reference;
+    private final Property<Piece> property;
+    private final Object value;
+    private final State state;
+
+    public Condition() {
+        this(new Reference(), new Property<>(null), null, null);
+    }
+
+    public Condition(Reference reference, Property<Piece> property, Object value, State state) {
+        this.reference = reference;
+        this.property = property;
+        this.value = value;
+        this.state = state;
+    }
+
+    public Boolean evaluate(Board board, Coordinate current, Coordinate next) {
+        if (this.property == null) {
+            return true;
+        }
+
+        Piece obj = this.getReferencePiece(board, current, next);
+        Object propertyValue = this.property.fetch(obj);
+        if (propertyValue == null || !propertyValue.getClass().equals(this.value.getClass())) {
+            return false;
+        }
+
+        switch (state) {
+            case TRUE -> {
+                return this.value.equals(true);
+            }
+            case FALSE -> {
+                return this.value.equals(false);
+            }
+            case EQUAL -> {
+                return this.value.equals(obj);
+            }
+            case OPPOSITE -> {
+                Piece currentPiece = board.getAt(current);
+                Object currentValue = this.property.fetch(currentPiece);
+                if(currentValue == null || !propertyValue.getClass().equals(currentValue.getClass())) {
+                    return false;
+                }
+                return !this.value.equals(currentValue);
+            }
+            default -> {
+                return false;
+            }
+        }
+    }
+
+    private Piece getReferencePiece(Board board, Coordinate current, Coordinate next) {
+        return switch (reference.getLocation()) {
+            case LAST_MOVED -> board.getLastMoved();
+            case AT_CURRENT -> board.getAt(current);
+            case AT_DESTINATION -> board.getAt(next);
+            case AT_COORDINATE -> board.getAt(reference.getCoordinate());
+        };
+    }
+}
+
