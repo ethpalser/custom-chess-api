3e0b46033f86645e3609254b95a3fd819fb7bef5
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2023-11-29 16:51:50 -0500

    [Hacky] Implemented Pawn 'En Passant' Movement
    Implemented En Passant for Pawn movement conditions. This is implemented roughly and will need to be refactored with other code refactoring.

--- src/main/java/com/chess/api/model/movement/condition/Condition.java
+++ src/main/java/com/chess/api/model/movement/condition/Condition.java
@@ -3,7 +3,6 @@ package com.chess.api.model.movement.condition;
 import com.chess.api.model.Board;
 import com.chess.api.model.Coordinate;
 import com.chess.api.model.piece.Piece;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import lombok.Builder;
@@ -15,16 +14,23 @@ public class Condition {
     private final Property<Piece> property;
     private final PropertyState propertyState;
     private final Object expected;
+    private final Reference compare; // Todo: Make this less hacky and prone to error
 
     public Condition() {
         this(new Reference(), new Property<>(null), null, null);
     }
 
     public Condition(Reference reference, Property<Piece> property, PropertyState propertyState, Object expected) {
+        this(reference, property, propertyState, expected, null);
+    }
+
+    public Condition(Reference reference, Property<Piece> property, PropertyState propertyState, Object expected,
+            Reference compare) {
         this.reference = reference;
         this.property = property;
         this.propertyState = propertyState;
         this.expected = expected;
+        this.compare = compare;
     }
 
     public Boolean evaluate(Board board, Coordinate start, Coordinate end) {
@@ -40,18 +46,19 @@ public class Condition {
                 continue;
             }
 
-            Object propVal = this.property.fetch(piece);
+            Object propVal = this.property != null ? this.property.fetch(piece) : null;
             switch (this.propertyState) {
                 case TRUE -> result = Boolean.TRUE.equals(propVal);
                 case FALSE -> result = Boolean.FALSE.equals(propVal);
                 case EQUAL -> result = (this.expected == null && propVal == null) ||
-                        this.expected != null && propVal != null
+                        (this.expected != null && propVal != null
                                 && propVal.getClass().equals(this.expected.getClass())
-                                && propVal.equals(this.expected);
+                                && propVal.equals(this.expected))
+                        || (this.compare != null && piece.equals(board.getReferencePiece(this.compare, start, end).get(0)));
                 case OPPOSITE -> {
                     // The start piece is implied for OPPOSITE, as the condition's value is not known until runtime
                     Piece currPiece = board.getPiece(start);
-                    Object currVal = this.property.fetch(currPiece);
+                    Object currVal = this.property != null ? this.property.fetch(currPiece) : null;
                     result = (currVal == null && propVal == null) ||
                             currVal != null && propVal != null
                                     && propVal.getClass().equals(currVal.getClass())
