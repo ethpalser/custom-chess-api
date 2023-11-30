95846445d17e718e0b60526c66308084824b85ed
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2023-11-29 19:20:39 -0500

    Created PieceFactory.java and Refactored Board.java
    Refactored Board.java to use HashMap for better space complexity. As it is now O(p) instead of O(l*w) where p is #pieces, l is the length and w is the width.
    Updated Board.java to use the new PieceFactory.java and cleaned up Piece.java to no longer have static methods that were for each known piece type.

--- src/main/java/com/chess/api/model/Board.java
+++ src/main/java/com/chess/api/model/Board.java
@@ -5,87 +5,136 @@ import com.chess.api.model.movement.Path;
 import com.chess.api.model.movement.condition.Condition;
 import com.chess.api.model.movement.condition.Reference;
 import com.chess.api.model.piece.Piece;
+import com.chess.api.model.piece.PieceFactory;
+import com.chess.api.model.piece.PieceType;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
-import lombok.Getter;
+import java.util.Map;
 import lombok.NonNull;
 
-@Getter
 public class Board {
 
-    private final Piece[][] pieces;
+    private final Map<Coordinate, Piece> pieceMap;
     private Piece lastMoved;
 
     public Board() {
-        Piece[][] pieceList = new Piece[8][8];
-
-        int y = 0;
-        for (int x = 0; x < 8; x++) {
-            pieceList[x][y] = (switch (x) {
-                case 0, 7 -> Piece.rook(Colour.WHITE, new Coordinate(x, y));
-                case 1, 6 -> Piece.knight(Colour.WHITE, new Coordinate(x, y));
-                case 2, 5 -> Piece.bishop(Colour.WHITE, new Coordinate(x, y));
-                case 3 -> Piece.queen(Colour.WHITE, new Coordinate(x, y));
-                case 4 -> Piece.king(Colour.WHITE, new Coordinate(x, y));
-                default -> throw new IllegalStateException("Unexpected value: " + x);
-            });
-        }
-
-        y = 1;
-        for (int x = 0; x < 8; x++) {
-            pieceList[x][y] = Piece.pawn(Colour.WHITE, new Coordinate(x, y));
-        }
-
-        y = 6;
-        for (int x = 0; x < 8; x++) {
-            pieceList[x][y] = Piece.pawn(Colour.BLACK, new Coordinate(x, y));
-        }
-
-        y = 7;
-        for (int x = 0; x < 8; x++) {
-            pieceList[x][y] = (switch (x) {
-                case 0, 7 -> Piece.rook(Colour.BLACK, new Coordinate(x, y));
-                case 1, 6 -> Piece.knight(Colour.BLACK, new Coordinate(x, y));
-                case 2, 5 -> Piece.bishop(Colour.BLACK, new Coordinate(x, y));
-                case 3 -> Piece.queen(Colour.BLACK, new Coordinate(x, y));
-                case 4 -> Piece.king(Colour.BLACK, new Coordinate(x, y));
-                default -> throw new IllegalStateException("Unexpected value: " + x);
-            });
-        }
-        this.pieces = pieceList;
+        Map<Coordinate, Piece> map = new HashMap<>();
+        map.putAll(this.generatePiecesInRank(0));
+        map.putAll(this.generatePiecesInRank(1));
+        map.putAll(this.generatePiecesInRank(Coordinate.MAX_Y - 1));
+        map.putAll(this.generatePiecesInRank(Coordinate.MAX_Y));
+        this.pieceMap = map;
         this.lastMoved = null;
     }
 
-    public int count() {
-        int count = 0;
-        for (int x = 0; x < pieces.length; x++) {
-            for (int y = 0; y < pieces[x].length; y++) {
-                if (pieces[x][y] != null)
-                    count++;
+    private Map<Coordinate, Piece> generatePiecesInRank(int y) {
+        Map<Coordinate, Piece> map = new HashMap<>();
+        Colour colour = y < Coordinate.MAX_Y / 2 ? Colour.WHITE : Colour.BLACK;
+
+        PieceFactory pieceFactory = PieceFactory.getInstance();
+        if (y == 0 || y == Coordinate.MAX_Y) {
+            for (int x = 0; x < 8; x++) {
+                Coordinate coordinate = new Coordinate(x, y);
+                Piece piece = switch (x) {
+                    case 0, 7 -> pieceFactory.build(PieceType.ROOK, colour, coordinate);
+                    case 1, 6 -> pieceFactory.build(PieceType.KNIGHT, colour, coordinate);
+                    case 2, 5 -> pieceFactory.build(PieceType.BISHOP, colour, coordinate);
+                    case 3 -> pieceFactory.build(PieceType.QUEEN, colour, coordinate);
+                    case 4 -> pieceFactory.build(PieceType.KING, colour, coordinate);
+                    default -> null;
+                };
+                map.put(coordinate, piece);
             }
+        } else if (y == 1 || y == Coordinate.MAX_Y - 1) {
+            for (int x = 0; x < 8; x++) {
+                Coordinate coordinate = new Coordinate(x, y);
+                Piece piece = pieceFactory.build(PieceType.PAWN, colour, coordinate);
+                map.put(coordinate, piece);
+            }
+        }
+        return map;
+    }
+
+    public int count() {
+        Collection<Piece> pieces = this.pieceMap.values();
+        int count = 0;
+        for (Piece piece : pieces) {
+            if (piece != null)
+                count++;
         }
         return count;
     }
 
+    public int length() {
+        return Coordinate.MAX_Y + 1;
+    }
+
+    public int width() {
+        return Coordinate.MAX_X + 1;
+    }
+
+    public void setPiece(@NonNull Coordinate coordinate, Piece piece) {
+        if (piece == null) {
+            this.pieceMap.remove(coordinate);
+        } else {
+            this.pieceMap.put(coordinate, piece);
+        }
+    }
+
     public Piece getPiece(int x, int y) {
-        return pieces[x][y];
+        if (x < 0 || x > Coordinate.MAX_X || y < 0 || y > Coordinate.MAX_Y) {
+            return null;
+        }
+        return pieceMap.get(Coordinate.at(x, y));
     }
 
     public Piece getPiece(Coordinate coordinate) {
-        return pieces[coordinate.getX()][coordinate.getY()];
+        if (coordinate == null) {
+            return null;
+        }
+        return pieceMap.get(coordinate);
     }
 
-    public List<Piece> getPieces(Coordinate source, Coordinate target) {
-        Path path = new Path(source, target);
-        List<Piece> list = new ArrayList<>();
-        for (Coordinate coordinate : path) {
-            if (this.getPiece(coordinate) != null) {
-                list.add(this.getPiece(coordinate));
-            }
-        }
-        return list;
+    public List<Piece> getPieces() {
+        return List.of();
     }
+
+    public List<Piece> getPieces(Path path) {
+        if (path == null) {
+            return this.getPieces();
+        }
+        List<Piece> pieceList = new LinkedList<>();
+        for (Coordinate coordinate : path) {
+            pieceList.add(this.getPiece(coordinate));
+        }
+        return pieceList;
+    }
+
+    public Piece getLastMoved() {
+        return lastMoved;
+    }
+
+    public List<Piece> getReferencePieces(Reference reference, Coordinate pathStart, Coordinate pathEnd) {
+        if (reference == null) {
+            return List.of();
+        }
+        List<Piece> pieces = new ArrayList<>();
+        switch (reference.location()) {
+            case LAST_MOVED -> pieces.add(this.getLastMoved());
+            case AT_START -> pieces.add(this.getPiece(pathStart));
+            case AT_DESTINATION -> pieces.add(this.getPiece(pathEnd));
+            case AT_COORDINATE -> pieces.add(this.getPiece(reference.coordinate()));
+            case PATH_TO_DESTINATION -> pieces = this.getPieces(new Path(pathStart, pathEnd));
+            case PATH_TO_COORDINATE -> pieces = this.getPieces(new Path(pathStart, reference.coordinate()));
+            case BELOW_DESTINATION -> pieces.add(this.getPiece(pathEnd.getX(), pathEnd.getY() - 1));
+        }
+        return pieces;
+    }
+
 
     /**
      * Move a piece to a new coordinate within the board.
@@ -101,8 +150,8 @@ public class Board {
      * @param end
      */
     public void movePiece(@NonNull Coordinate start, @NonNull Coordinate end) {
-        Piece source = pieces[start.getX()][start.getY()];
-        Piece target = pieces[end.getX()][end.getY()];
+        Piece source = this.getPiece(start);
+        Piece target = this.getPiece(end);
 
         if (source == null || source.equals(target) || (target != null && source.getColour().equals(target.getColour()))) {
             // Cannot move a piece if there is no piece, both are the same piece, or both are the same colour
@@ -125,7 +174,8 @@ public class Board {
                     break;
                 }
             }
-            hasValidPath = hasValidConditions && movement.isValidCoordinate(source.getColour(), start, end) && this.isValidPath(movement.getPath(source.getColour(), start, end));
+            hasValidPath =
+                    hasValidConditions && movement.isValidCoordinate(source.getColour(), start, end) && this.isValidPath(movement.getPath(source.getColour(), start, end));
         }
         if (movement == null || !hasValidPath) {
             // Piece is in the middle of the path
@@ -134,9 +184,9 @@ public class Board {
         // Update the piece's internal position
         source.performMove(end);
         // Update the piece on the board
-        pieces[end.getX()][end.getY()] = pieces[start.getX()][start.getY()];
-        pieces[start.getX()][start.getY()] = null;
-        this.lastMoved = pieces[end.getX()][end.getY()];
+        this.pieceMap.put(end, source);
+        this.pieceMap.remove(start);
+        this.lastMoved = source;
         if (movement.getExtraMovement() != null) {
             movement.getExtraMovement().move(this, start);
         }
@@ -158,31 +208,14 @@ public class Board {
         return true;
     }
 
-    public List<Piece> getReferencePiece(Reference reference, Coordinate start, Coordinate end) {
-        if (reference == null) {
-            return List.of();
-        }
-        List<Piece> pieces = new ArrayList<>();
-        switch (reference.location()) {
-            case LAST_MOVED -> pieces.add(this.getLastMoved());
-            case AT_START -> pieces.add(this.getPiece(start));
-            case AT_DESTINATION -> pieces.add(this.getPiece(end));
-            case AT_COORDINATE -> pieces.add(this.getPiece(reference.coordinate()));
-            case PATH_TO_DESTINATION -> pieces = this.getPieces(start, end);
-            case PATH_TO_COORDINATE -> pieces = this.getPieces(start, reference.coordinate());
-            case BELOW_DESTINATION -> pieces.add(this.getPiece(end.getX(), end.getY() - 1));
-        }
-        return pieces;
-    }
-
     public void updatePieceLocation(@NonNull Coordinate start, Coordinate destination) {
-        if (pieces[start.getX()][start.getY()] == null) {
+        if (this.getPiece(start) == null) {
             return;
         }
         if (destination != null) {
-            pieces[destination.getX()][destination.getY()] = pieces[start.getX()][start.getY()];
+            this.setPiece(destination, this.getPiece(start));
         }
-        pieces[start.getX()][start.getY()] = null;
+        this.setPiece(start, null);
     }
 
 }
