771f61bbc52a5914e135ca5fea229753cba3be0e
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2023-12-02 00:30:35 -0500

    Added comments to methods
    Added comments to methods that are not intuitive on its own, or that it contains some complexity. Overridden, Getter and Setter methods do not have comments.

--- src/main/java/com/chess/api/game/condition/Comparator.java
+++ src/main/java/com/chess/api/game/condition/Comparator.java
@@ -8,6 +8,13 @@ public enum Comparator {
     EXIST,
     DOES_NOT_EXIST;
 
+    /**
+     * This uses the given Comparator to return if it is allowed for Conditionals that only have one Reference, or
+     * that this only requires the Reference itself.
+     *
+     * @param comparator {@link Comparator}
+     * @return boolean
+     */
     public static boolean canReferenceSelf(Comparator comparator) {
         return switch (comparator) {
             case FALSE, TRUE, EXIST, DOES_NOT_EXIST -> true;
--- src/main/java/com/chess/api/game/movement/ExtraAction.java
+++ src/main/java/com/chess/api/game/movement/ExtraAction.java
@@ -6,6 +6,10 @@ import com.chess.api.game.piece.Piece;
 import com.chess.api.game.reference.Reference;
 import lombok.NonNull;
 
+/**
+ * Container of a follow-up movement with a reference to where the piece is relative to the previous action and
+ * Vector2D of where it will go.
+ */
 public class ExtraAction {
 
     private final Reference reference;
@@ -21,6 +25,13 @@ public class ExtraAction {
         this.destination = destination;
     }
 
+    /**
+     * Creates an Action using a previous action and this reference for the board to consume for another movement.
+     *
+     * @param board {@link Board} needed for {@link Reference} to refer to
+     * @param previousAction {@link Action} that this ExtraAction is following-up on
+     * @return {@link Action}
+     */
     public Action getAction(@NonNull Board board, @NonNull Action previousAction) {
         if (this.reference == null) {
             return null;
