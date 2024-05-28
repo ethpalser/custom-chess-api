0be165e508991090bb796a7f511e4b62bba91d22
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-05-27 23:01:27 -0400

    Added queries to Repositories

--- src/main/java/com/chess/api/dao/UserRepository.java
+++ src/main/java/com/chess/api/dao/UserRepository.java
@@ -2,6 +2,15 @@ package com.chess.api.dao;
 
 import com.chess.api.data.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface UserRepository extends MongoRepository<User, Long> {
+
+    @Query("{_id:$eq{'?0'}}")
+    User findUserById(long id);
+
+    @Query("{token:$eq{'?0'}}")
+    User findUserByToken(String token);
+
+
 }
