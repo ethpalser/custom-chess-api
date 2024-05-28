8049f6ca42ad47cdfe2a8d717dd7fdd336302dc1
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-05-27 23:01:27 -0400

    Added queries to Repositories

--- src/main/java/com/chess/api/dao/SessionRepository.java
+++ src/main/java/com/chess/api/dao/SessionRepository.java
@@ -1,7 +1,16 @@
 package com.chess.api.dao;
 
 import com.chess.api.data.Session;
+import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface SessionRepository extends MongoRepository<Session, Long> {
+
+    @Query("{_id:$eq{'?0'}}")
+    Session findSessionById(long id);
+
+    @Query("{inProgress:$eq{'?0'}}")
+    List<Session> findAllByInProgress(boolean inProgress);
+
 }
