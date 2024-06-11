69949c2c4f128ab6c56e003bf6156d2453306184
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-06-10 22:56:15 -0400

    Removed IDs from Views

--- src/main/java/com/chess/api/view/response/PieceView.java
+++ src/main/java/com/chess/api/view/response/PieceView.java
@@ -1,13 +1,12 @@
 package com.chess.api.view.response;
 
 import java.util.Set;
-import java.util.UUID;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class PieceView {
 
-    private final UUID id;
+    private final String code;
     private final String colour;
     private final Set<PointView> allowedMoves;
 
--- src/main/java/com/chess/api/view/response/SessionView.java
+++ src/main/java/com/chess/api/view/response/SessionView.java
@@ -2,13 +2,11 @@ package com.chess.api.view.response;
 
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class SessionView {
 
-    private final UUID id;
     private final PlayerView playerWhite;
     private final PlayerView playerBlack;
     private final List<ActionView> moves;
