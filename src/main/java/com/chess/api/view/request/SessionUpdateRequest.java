0246fcb332e910e527a3573d4ff9e61b1f9e5213
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-06-17 22:19:27 -0400

    Added Getter to requests

--- src/main/java/com/chess/api/view/request/SessionUpdateRequest.java
+++ src/main/java/com/chess/api/view/request/SessionUpdateRequest.java
@@ -2,8 +2,10 @@ package com.chess.api.view.request;
 
 import com.chess.api.data.piece.Action;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class SessionUpdateRequest {
 
     private final Action action;
--- src/main/java/com/chess/api/view/request/UserUpdateRequest.java
+++ src/main/java/com/chess/api/view/request/UserUpdateRequest.java
@@ -1,8 +1,10 @@
 package com.chess.api.view.request;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class UserUpdateRequest {
 
     private final String newUsername;
