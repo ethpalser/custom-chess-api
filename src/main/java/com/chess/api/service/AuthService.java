3047c69b92a207b6e83260bf269387883f0bb3c5
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-06-19 22:23:50 -0400

    Added AuthService
    Added service to generate JWT and eventually refresh tokens

---
+++ src/main/java/com/chess/api/service/AuthService.java
@@ -0,0 +1,57 @@
+package com.chess.api.service;
+
+import com.auth0.jwt.JWTCreator;
+import com.auth0.jwt.algorithms.Algorithm;
+import java.security.spec.RSAPublicKeySpec;
+import java.util.Date;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import com.auth0.jwt.JWT;
+
+@Service
+public class AuthService {
+
+    private static final String JWT_ISS = "www.custom-chess-api.com";
+
+    private static final String JWT_SUB = "www.custom-chess-client.com";
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public final String createJWT(Date issueExp) {
+        return createJWT(JWT_ISS, JWT_SUB, issueExp);
+    }
+
+    public final String createJWT(String issuer, String subject, Date issueExp) {
+        Algorithm alg = Algorithm.HMAC256(this.secret);
+
+        JWTCreator.Builder tokenBuilder = JWT.create()
+                .withIssuer(issuer)
+                .withSubject(subject)
+                .withExpiresAt(issueExp);
+
+        return tokenBuilder.sign(alg);
+    }
+
+    @SafeVarargs
+    public final String createJWTWithClaims(Date issueExp, Map.Entry<String, String>... claims) {
+        return createJWTWithClaims(JWT_ISS, JWT_SUB, issueExp, claims);
+    }
+
+    @SafeVarargs
+    public final String createJWTWithClaims(String issuer, String subject, Date issueExp, Map.Entry<String, String>... claims) {
+        Algorithm alg = Algorithm.HMAC256(this.secret);
+
+        JWTCreator.Builder tokenBuilder = JWT.create()
+                .withIssuer(issuer)
+                .withSubject(subject)
+                .withExpiresAt(issueExp);
+
+         for (Map.Entry<String, String> c : claims) {
+             tokenBuilder.withClaim(c.getKey(), c.getValue());
+         }
+         return tokenBuilder.sign(alg);
+    }
+
+}
