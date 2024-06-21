40de2a7ca02a274afac40798ae240ac3ffbf19e4
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-06-21 00:01:57 -0400

    Updated AuthService to use new KeyValuePair

--- src/main/java/com/chess/api/service/AuthService.java
+++ src/main/java/com/chess/api/service/AuthService.java
@@ -1,14 +1,13 @@
 package com.chess.api.service;
 
+import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.chess.api.core.utils.KeyValuePair;
 import java.nio.charset.StandardCharsets;
-import java.security.spec.RSAPublicKeySpec;
 import java.util.Date;
-import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import com.auth0.jwt.JWT;
 
 @Service
 public class AuthService {
@@ -36,12 +35,13 @@ public class AuthService {
     }
 
     @SafeVarargs
-    public final String createJWTWithClaims(Date issueExp, Map.Entry<String, String>... claims) {
+    public final String createJWTWithClaims(Date issueExp, KeyValuePair<String, String>... claims) {
         return createJWTWithClaims(JWT_ISS, JWT_SUB, issueExp, claims);
     }
 
     @SafeVarargs
-    public final String createJWTWithClaims(String issuer, String subject, Date issueExp, Map.Entry<String, String>... claims) {
+    public final String createJWTWithClaims(String issuer, String subject, Date issueExp,
+            KeyValuePair<String, String>... claims) {
         Algorithm alg = Algorithm.HMAC256(this.secret);
 
         JWTCreator.Builder tokenBuilder = JWT.create()
@@ -49,16 +49,16 @@ public class AuthService {
                 .withSubject(subject)
                 .withExpiresAt(issueExp);
 
-         for (Map.Entry<String, String> c : claims) {
-             tokenBuilder.withClaim(c.getKey(), c.getValue());
-         }
-         return tokenBuilder.sign(alg);
+        for (KeyValuePair<String, String> c : claims) {
+            tokenBuilder.withClaim(c.key, c.value);
+        }
+        return tokenBuilder.sign(alg);
     }
 
     public String createRefreshToken() {
         byte[] bytes = new byte[32];
         for (int i = 0; i < bytes.length; i++) {
-            bytes[i] = (byte)Math.floor(Math.random() * 128);
+            bytes[i] = (byte) Math.floor(Math.random() * 128);
         }
         return new String(bytes, StandardCharsets.UTF_8);
     }
