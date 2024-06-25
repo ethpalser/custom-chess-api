8f25d182b4ef7ef7efc90eb09591bc2fb8da4db1
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-06-25 00:26:38 -0400

    Update ApiControllerAdvice.java

--- src/main/java/com/chess/api/ApiControllerAdvice.java
+++ src/main/java/com/chess/api/ApiControllerAdvice.java
@@ -1,7 +1,11 @@
 package com.chess.api;
 
 import com.chess.api.core.exception.UnauthorizedAccessException;
+import com.chess.api.view.response.ErrorView;
+import jakarta.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,7 +15,17 @@ public class ApiControllerAdvice {
 
     @ExceptionHandler(UnauthorizedAccessException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public void handleUnauthorizedAccess(UnauthorizedAccessException e) {
+    public ResponseEntity<ErrorView> handleUnauthorizedAccess(
+            HttpServletRequest request,
+            UnauthorizedAccessException e) {
+        final ErrorView response = new ErrorView(
+                "Unauthorized access",
+                e.getErrorCode(),
+                e.getMessage(),
+                request.getServletPath(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
     }
 
 }
