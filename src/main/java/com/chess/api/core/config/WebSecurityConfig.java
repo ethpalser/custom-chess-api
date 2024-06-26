d5b34576848ae56e0a271627490955c81042402e
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2024-06-26 15:54:36 -0400

    Replaced basic commons logging filter

--- src/main/java/com/chess/api/core/config/WebSecurityConfig.java
+++ src/main/java/com/chess/api/core/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.chess.api.core.security;
+package com.chess.api.core.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
